b1ba43ce9dea11996697cd3cb66cfdf2155e334a
Eric Fischer <enf@pobox.com>
2013-03-02 23:09:37 -0800

    Reindent voronoi code to make it somewhat readable.
    indent -kr -i8 -brf

--- voronoi/main.c
+++ voronoi/main.c
@@ -1,162 +1,147 @@
 /*** MAIN.C ***/
 
 #include <stdio.h>
-#include <stdlib.h>  /* realloc(), qsort() */
+#include <stdlib.h>		/* realloc(), qsort() */
 #include <string.h>
 #include <math.h>
 
 #include "vdefs.h"
 
-Site * nextone(void) ;
-void readsites(void) ;
+Site *nextone(void);
+void readsites(void);
 
-int triangulate, plot, debug, nsites, siteidx ;
-double xmin, xmax, ymin, ymax ;
-Site * sites ;
-Freelist sfl ;
+int triangulate, plot, debug, nsites, siteidx;
+double xmin, xmax, ymin, ymax;
+Site *sites;
+Freelist sfl;
 
-int
-main(int argc, char *argv[])
-    {
-    int c ;
+int main(int argc, char *argv[]) {
+	int c;
 
-    triangulate = debug = 0 ;
-    plot = 1;
-    while ((c = getopt(argc, argv, "dpt")) != EOF)
-        {
-        switch(c)
-            {
-            case 'd':
-                debug = 1 ;
-                break ;
-            case 't':
-                triangulate = 1 ;
-		plot = 0 ;
-                break ;
-            case 'p':
-                plot = 1 ;
-                break ;
-            }
-        }
+	triangulate = debug = 0;
+	plot = 1;
+	while ((c = getopt(argc, argv, "dpt")) != EOF) {
+		switch (c) {
+		case 'd':
+			debug = 1;
+			break;
+		case 't':
+			triangulate = 1;
+			plot = 0;
+			break;
+		case 'p':
+			plot = 1;
+			break;
+		}
+	}
 
-    freeinit(&sfl, sizeof(Site)) ;
-    readsites() ;
-    siteidx = 0 ;
-    geominit() ;
-    if (plot)
-        {
-        plotinit() ;
-        }
-    voronoi(nextone) ;
-    return (0) ;
-    }
+	freeinit(&sfl, sizeof(Site));
+	readsites();
+	siteidx = 0;
+	geominit();
+	if (plot) {
+		plotinit();
+	}
+	voronoi(nextone);
+	return (0);
+}
 
 /*** sort sites on y, then x, coord ***/
 
-int
-scomp(const void * vs1, const void * vs2)
-    {
-    Point * s1 = (Point *)vs1 ;
-    Point * s2 = (Point *)vs2 ;
+int scomp(const void *vs1, const void *vs2) {
+	Point *s1 = (Point *) vs1;
+	Point *s2 = (Point *) vs2;
 
-    if (s1->y < s2->y)
-        {
-        return (-1) ;
-        }
-    if (s1->y > s2->y)
-        {
-        return (1) ;
-        }
-    if (s1->x < s2->x)
-        {
-        return (-1) ;
-        }
-    if (s1->x > s2->x)
-        {
-        return (1) ;
-        }
-    return (0) ;
-    }
+	if (s1->y < s2->y) {
+		return (-1);
+	}
+	if (s1->y > s2->y) {
+		return (1);
+	}
+	if (s1->x < s2->x) {
+		return (-1);
+	}
+	if (s1->x > s2->x) {
+		return (1);
+	}
+	return (0);
+}
 
 /*** return a single in-storage site ***/
 
-Site *
-nextone(void)
-    {
-    Site * s ;
+Site *nextone(void) {
+	Site *s;
 
-    if (siteidx < nsites)
-        {
-        s = &sites[siteidx++];
-        return (s) ;
-        }
-    else
-        {
-        return ((Site *)NULL) ;
-        }
-    }
+	if (siteidx < nsites) {
+		s = &sites[siteidx++];
+		return (s);
+	} else {
+		return ((Site *) NULL);
+	}
+}
 
 
 /*** read all sites, sort, and compute xmin, xmax, ymin, ymax ***/
 
-void
-readsites(void)
-    {
-    int i ;
-    char s[2000];
-    char user[2000], date[2000], time[2000];
+void readsites(void) {
+	int i;
+	char s[2000];
+	char user[2000], date[2000], time[2000];
 
-    nsites = 0 ;
-    sites = (Site *) myalloc(4000 * sizeof(Site));
-    while (fgets(s, 2000, stdin))
-        {
-	if (sscanf(s, "%s %s %s %lf,%lf", user, date, time, &sites[nsites].coord.x, &sites[nsites].coord.y) != 5)
-	    {
-	    if (sscanf(s, "%lf %lf", &sites[nsites].coord.x, &sites[nsites].coord.y) != 2)
-		{
-		fprintf(stderr, "Don't know what to do with %s", s);
-		continue;
+	nsites = 0;
+	sites = (Site *) myalloc(4000 * sizeof(Site));
+	while (fgets(s, 2000, stdin)) {
+		if (sscanf
+		    (s, "%s %s %s %lf,%lf", user, date, time,
+		     &sites[nsites].coord.x,
+		     &sites[nsites].coord.y) != 5) {
+			if (sscanf
+			    (s, "%lf %lf", &sites[nsites].coord.x,
+			     &sites[nsites].coord.y) != 2) {
+				fprintf(stderr,
+					"Don't know what to do with %s",
+					s);
+				continue;
+			}
 		}
-	    }
-	sites[nsites].text = strdup(s);
-        sites[nsites].sitenbr = nsites ;
-        sites[nsites++].refcnt = 0 ;
-        if (nsites % 4000 == 0)
-            sites = (Site *)
-realloc(sites,(nsites+4000)*sizeof(Site));
-        }
+		sites[nsites].text = strdup(s);
+		sites[nsites].sitenbr = nsites;
+		sites[nsites++].refcnt = 0;
+		if (nsites % 4000 == 0)
+			sites = (Site *)
+			    realloc(sites, (nsites + 4000) * sizeof(Site));
+	}
 
-    qsort((void *)sites, nsites, sizeof(Site), scomp) ;
+	qsort((void *) sites, nsites, sizeof(Site), scomp);
 
 #define SMALL .000001
 
-    int out = 0;
-    for (i = 1; i < nsites; i++) {
-	if (fabs(sites[i].coord.x - sites[i - 1].coord.x) > SMALL ||
-	    fabs(sites[i].coord.y - sites[i - 1].coord.y) > SMALL) {
-		sites[out] = sites[i];
-		out++;
-	} else {
-		fprintf(stderr, "discard dup %f %f of %f %f\n", sites[i].coord.x, sites[i].coord.y,
-								sites[i - 1].coord.x, sites[i - 1].coord.y);
+	int out = 0;
+	for (i = 1; i < nsites; i++) {
+		if (fabs(sites[i].coord.x - sites[i - 1].coord.x) > SMALL
+		    || fabs(sites[i].coord.y - sites[i - 1].coord.y) >
+		    SMALL) {
+			sites[out] = sites[i];
+			out++;
+		} else {
+			fprintf(stderr, "discard dup %f %f of %f %f\n",
+				sites[i].coord.x, sites[i].coord.y,
+				sites[i - 1].coord.x,
+				sites[i - 1].coord.y);
+		}
 	}
-    }
-    nsites = out;
+	nsites = out;
 
-    xmin = sites[0].coord.x ;
-    xmax = sites[0].coord.x ;
-    for (i = 1 ; i < nsites ; ++i)
-        {
-        if(sites[i].coord.x < xmin)
-            {
-            xmin = sites[i].coord.x ;
-            }
-        if (sites[i].coord.x > xmax)
-            {
-            xmax = sites[i].coord.x ;
-            }
-        }
-    ymin = sites[0].coord.y ;
-    ymax = sites[nsites-1].coord.y ;
-    }
-
+	xmin = sites[0].coord.x;
+	xmax = sites[0].coord.x;
+	for (i = 1; i < nsites; ++i) {
+		if (sites[i].coord.x < xmin) {
+			xmin = sites[i].coord.x;
+		}
+		if (sites[i].coord.x > xmax) {
+			xmax = sites[i].coord.x;
+		}
+	}
+	ymin = sites[0].coord.y;
+	ymax = sites[nsites - 1].coord.y;
+}
--- voronoi/output.c
+++ voronoi/output.c
@@ -7,292 +7,253 @@
 
 #include "vdefs.h"
 
-extern int triangulate, plot, debug ;
-extern double ymax, ymin, xmax, xmin ;
+extern int triangulate, plot, debug;
+extern double ymax, ymin, xmax, xmin;
 
 double pxmin, pxmax, pymin, pymax, cradius;
 
-void
-openpl(void)
-    {
-    }
+void openpl(void) {
+}
 
 #pragma argsused
-void
-line(double ax, double ay, double bx, double by)
-    {
-	printf("%.6f %.6f moveto %.6f %.6f lineto stroke\n", ax, ay, bx, by);
-    }
+void line(double ax, double ay, double bx, double by) {
+	printf("%.6f %.6f moveto %.6f %.6f lineto stroke\n", ax, ay, bx,
+	       by);
+}
 
 #pragma argsused
-void
-circle(double ax, double ay, double radius)
-    {
-    }
+void circle(double ax, double ay, double radius) {
+}
 
 #pragma argsused
-void
-range(double pxmin, double pxmax, double pymin, double pymax)
-    {
-    }
+void range(double pxmin, double pxmax, double pymin, double pymax) {
+}
 
-void
-out_bisector(Edge * e)
-    {
-    if (triangulate && plot && !debug)
-        {
-        line(e->reg[0]->coord.x, e->reg[0]->coord.y,
-             e->reg[1]->coord.x, e->reg[1]->coord.y) ;
-        }
-    if (!triangulate && !plot && !debug)
-        {
-        printf("l %f %f %f\n", e->a, e->b, e->c) ;
-        }
-    if (debug)
-        {
-        printf("line(%d) %gx+%gy=%g, bisecting %d %d\n", e->edgenbr,
-        e->a, e->b, e->c, e->reg[le]->sitenbr, e->reg[re]->sitenbr) ;
-        }
-    }
+void out_bisector(Edge * e) {
+	if (triangulate && plot && !debug) {
+		line(e->reg[0]->coord.x, e->reg[0]->coord.y,
+		     e->reg[1]->coord.x, e->reg[1]->coord.y);
+	}
+	if (!triangulate && !plot && !debug) {
+		printf("l %f %f %f\n", e->a, e->b, e->c);
+	}
+	if (debug) {
+		printf("line(%d) %gx+%gy=%g, bisecting %d %d\n",
+		       e->edgenbr, e->a, e->b, e->c, e->reg[le]->sitenbr,
+		       e->reg[re]->sitenbr);
+	}
+}
 
-void
-out_ep(Edge * e)
-    {
-    if (!triangulate && plot)
-        {
+void out_ep(Edge * e) {
+	if (!triangulate && plot) {
 #if 0
-	printf("ep: endpoints: ");
-        printf(" %d ", e->ep[le] != (Site *)NULL ? e->ep[le]->sitenbr : -1) ;
-        printf("%d", e->ep[re] != (Site *)NULL ? e->ep[re]->sitenbr : -1) ;
-	printf("\n");
-	printf("ep: regions: ");
-        printf(" %d ", e->reg[le] != (Site *)NULL ? e->reg[le]->sitenbr : -1) ;
-        printf("%d ", e->reg[re] != (Site *)NULL ? e->reg[re]->sitenbr : -1) ;
-	printf(" region points: ");
-        printf("%f %f %f %f\n", e->reg[0]->coord.x, e->reg[0]->coord.y,
-             e->reg[1]->coord.x, e->reg[1]->coord.y) ;
+		printf("ep: endpoints: ");
+		printf(" %d ",
+		       e->ep[le] !=
+		       (Site *) NULL ? e->ep[le]->sitenbr : -1);
+		printf("%d",
+		       e->ep[re] !=
+		       (Site *) NULL ? e->ep[re]->sitenbr : -1);
+		printf("\n");
+		printf("ep: regions: ");
+		printf(" %d ",
+		       e->reg[le] !=
+		       (Site *) NULL ? e->reg[le]->sitenbr : -1);
+		printf("%d ",
+		       e->reg[re] !=
+		       (Site *) NULL ? e->reg[re]->sitenbr : -1);
+		printf(" region points: ");
+		printf("%f %f %f %f\n", e->reg[0]->coord.x,
+		       e->reg[0]->coord.y, e->reg[1]->coord.x,
+		       e->reg[1]->coord.y);
 #endif
 
 
-        clip_line(e) ;
+		clip_line(e);
 
-        }
-    if (!triangulate && !plot)
-        {
-        printf("e %d", e->edgenbr);
-        printf(" %d ", e->ep[le] != (Site *)NULL ? e->ep[le]->sitenbr : -1) ;
-        printf("%d\n", e->ep[re] != (Site *)NULL ? e->ep[re]->sitenbr : -1) ;
-        }
-    }
+	}
+	if (!triangulate && !plot) {
+		printf("e %d", e->edgenbr);
+		printf(" %d ",
+		       e->ep[le] !=
+		       (Site *) NULL ? e->ep[le]->sitenbr : -1);
+		printf("%d\n",
+		       e->ep[re] !=
+		       (Site *) NULL ? e->ep[re]->sitenbr : -1);
+	}
+}
 
-void
-out_vertex(Site * v)
-    {
-    if (!triangulate && !plot && !debug)
-        {
-        printf ("v %f %f\n", v->coord.x, v->coord.y) ;
-        }
-    if (debug)
-        {
-        printf("vertex(%d) at %f %f\n", v->sitenbr, v->coord.x, v->coord.y) ;
-        }
-    }
+void out_vertex(Site * v) {
+	if (!triangulate && !plot && !debug) {
+		printf("v %f %f\n", v->coord.x, v->coord.y);
+	}
+	if (debug) {
+		printf("vertex(%d) at %f %f\n", v->sitenbr, v->coord.x,
+		       v->coord.y);
+	}
+}
 
-void
-out_site(Site * s)
-    {
-    if (!triangulate && plot && !debug)
-        {
-        circle (s->coord.x, s->coord.y, cradius) ;
-        }
-    if (!triangulate && !plot && !debug)
-        {
-        printf("s %f %f\n", s->coord.x, s->coord.y) ;
-        }
-    if (debug)
-        {
-        printf("site (%d) at %f %f\n", s->sitenbr, s->coord.x, s->coord.y) ;
-        }
-    }
+void out_site(Site * s) {
+	if (!triangulate && plot && !debug) {
+		circle(s->coord.x, s->coord.y, cradius);
+	}
+	if (!triangulate && !plot && !debug) {
+		printf("s %f %f\n", s->coord.x, s->coord.y);
+	}
+	if (debug) {
+		printf("site (%d) at %f %f\n", s->sitenbr, s->coord.x,
+		       s->coord.y);
+	}
+}
 
-void
-out_triple(Site * s1, Site * s2, Site * s3)
-    {
-    if (triangulate && !plot && !debug)
-        {
+void out_triple(Site * s1, Site * s2, Site * s3) {
+	if (triangulate && !plot && !debug) {
 #if 0
-        printf("%d %d %d\n", s1->sitenbr, s2->sitenbr, s3->sitenbr) ;
+		printf("%d %d %d\n", s1->sitenbr, s2->sitenbr,
+		       s3->sitenbr);
 #endif
-        printf("%d %f,%f %d %f,%f %d %f,%f\n",
-		s1->sitenbr, s1->coord.x, s1->coord.y,
-		s2->sitenbr, s2->coord.x, s2->coord.y,
-		s3->sitenbr, s3->coord.x, s3->coord.y) ;
-        }
-    if (debug)
-        {
-        printf("circle through left=%d right=%d bottom=%d\n",
-        s1->sitenbr, s2->sitenbr, s3->sitenbr) ;
-        }
-    }
+		printf("%d %f,%f %d %f,%f %d %f,%f\n",
+		       s1->sitenbr, s1->coord.x, s1->coord.y,
+		       s2->sitenbr, s2->coord.x, s2->coord.y,
+		       s3->sitenbr, s3->coord.x, s3->coord.y);
+	}
+	if (debug) {
+		printf("circle through left=%d right=%d bottom=%d\n",
+		       s1->sitenbr, s2->sitenbr, s3->sitenbr);
+	}
+}
 
-void
-plotinit(void)
-    {
-    double dx, dy, d ;
+void plotinit(void) {
+	double dx, dy, d;
 
-    dy = ymax - ymin ;
-    dx = xmax - xmin ;
-    d = ( dx > dy ? dx : dy) * 1.1 ;
-    pxmin = xmin - (d-dx) / 2.0 ;
-    pxmax = xmax + (d-dx) / 2.0 ;
-    pymin = ymin - (d-dy) / 2.0 ;
-    pymax = ymax + (d-dy) / 2.0 ;
-    cradius = (pxmax - pxmin) / 350.0 ;
-    openpl() ;
-    range(pxmin, pymin, pxmax, pymax) ;
-    }
+	dy = ymax - ymin;
+	dx = xmax - xmin;
+	d = (dx > dy ? dx : dy) * 1.1;
+	pxmin = xmin - (d - dx) / 2.0;
+	pxmax = xmax + (d - dx) / 2.0;
+	pymin = ymin - (d - dy) / 2.0;
+	pymax = ymax + (d - dy) / 2.0;
+	cradius = (pxmax - pxmin) / 350.0;
+	openpl();
+	range(pxmin, pymin, pxmax, pymax);
+}
 
-void
-clip_line(Edge * e)
-    {
-    Site * s1, * s2 ;
-    double x1, x2, y1, y2 ;
+void clip_line(Edge * e) {
+	Site *s1, *s2;
+	double x1, x2, y1, y2;
 
-    if (e->a == 1.0 && e->b >= 0.0)
-        {
-        s1 = e->ep[1] ;
-        s2 = e->ep[0] ;
-        }
-    else
-        {
-        s1 = e->ep[0] ;
-        s2 = e->ep[1] ;
-        }
-    if (e->a == 1.0)
-        {
-        y1 = pymin ;
-        if (s1 != (Site *)NULL && s1->coord.y > pymin)
-            {
-             y1 = s1->coord.y ;
-             }
-        if (y1 > pymax)
-            {
-            return ;
-            }
-        x1 = e->c - e->b * y1 ;
-        y2 = pymax ;
-        if (s2 != (Site *)NULL && s2->coord.y < pymax)
-            {
-            y2 = s2->coord.y ;
-            }
-        if (y2 < pymin)
-            {
-            return ;
-            }
-        x2 = e->c - e->b * y2 ;
-        if (((x1 > pxmax) && (x2 > pxmax)) || ((x1 < pxmin) && (x2 < pxmin)))
-            {
-            return ;
-            }
-        if (x1 > pxmax)
-            {
-            x1 = pxmax ;
-            y1 = (e->c - x1) / e->b ;
-            }
-        if (x1 < pxmin)
-            {
-            x1 = pxmin ;
-            y1 = (e->c - x1) / e->b ;
-            }
-        if (x2 > pxmax)
-            {
-            x2 = pxmax ;
-            y2 = (e->c - x2) / e->b ;
-            }
-        if (x2 < pxmin)
-            {
-            x2 = pxmin ;
-            y2 = (e->c - x2) / e->b ;
-            }
-        }
-    else
-        {
-        x1 = pxmin ;
-        if (s1 != (Site *)NULL && s1->coord.x > pxmin)
-            {
-            x1 = s1->coord.x ;
-            }
-        if (x1 > pxmax)
-            {
-            return ;
-            }
-        y1 = e->c - e->a * x1 ;
-        x2 = pxmax ;
-        if (s2 != (Site *)NULL && s2->coord.x < pxmax)
-            {
-            x2 = s2->coord.x ;
-            }
-        if (x2 < pxmin)
-            {
-            return ;
-            }
-        y2 = e->c - e->a * x2 ;
-        if (((y1 > pymax) && (y2 > pymax)) || ((y1 < pymin) && (y2 <pymin)))
-            {
-            return ;
-            }
-        if (y1> pymax)
-            {
-            y1 = pymax ;
-            x1 = (e->c - y1) / e->a ;
-            }
-        if (y1 < pymin)
-            {
-            y1 = pymin ;
-            x1 = (e->c - y1) / e->a ;
-            }
-        if (y2 > pymax)
-            {
-            y2 = pymax ;
-            x2 = (e->c - y2) / e->a ;
-            }
-        if (y2 < pymin)
-            {
-            y2 = pymin ;
-            x2 = (e->c - y2) / e->a ;
-            }
-        }
+	if (e->a == 1.0 && e->b >= 0.0) {
+		s1 = e->ep[1];
+		s2 = e->ep[0];
+	} else {
+		s1 = e->ep[0];
+		s2 = e->ep[1];
+	}
+	if (e->a == 1.0) {
+		y1 = pymin;
+		if (s1 != (Site *) NULL && s1->coord.y > pymin) {
+			y1 = s1->coord.y;
+		}
+		if (y1 > pymax) {
+			return;
+		}
+		x1 = e->c - e->b * y1;
+		y2 = pymax;
+		if (s2 != (Site *) NULL && s2->coord.y < pymax) {
+			y2 = s2->coord.y;
+		}
+		if (y2 < pymin) {
+			return;
+		}
+		x2 = e->c - e->b * y2;
+		if (((x1 > pxmax) && (x2 > pxmax))
+		    || ((x1 < pxmin) && (x2 < pxmin))) {
+			return;
+		}
+		if (x1 > pxmax) {
+			x1 = pxmax;
+			y1 = (e->c - x1) / e->b;
+		}
+		if (x1 < pxmin) {
+			x1 = pxmin;
+			y1 = (e->c - x1) / e->b;
+		}
+		if (x2 > pxmax) {
+			x2 = pxmax;
+			y2 = (e->c - x2) / e->b;
+		}
+		if (x2 < pxmin) {
+			x2 = pxmin;
+			y2 = (e->c - x2) / e->b;
+		}
+	} else {
+		x1 = pxmin;
+		if (s1 != (Site *) NULL && s1->coord.x > pxmin) {
+			x1 = s1->coord.x;
+		}
+		if (x1 > pxmax) {
+			return;
+		}
+		y1 = e->c - e->a * x1;
+		x2 = pxmax;
+		if (s2 != (Site *) NULL && s2->coord.x < pxmax) {
+			x2 = s2->coord.x;
+		}
+		if (x2 < pxmin) {
+			return;
+		}
+		y2 = e->c - e->a * x2;
+		if (((y1 > pymax) && (y2 > pymax))
+		    || ((y1 < pymin) && (y2 < pymin))) {
+			return;
+		}
+		if (y1 > pymax) {
+			y1 = pymax;
+			x1 = (e->c - y1) / e->a;
+		}
+		if (y1 < pymin) {
+			y1 = pymin;
+			x1 = (e->c - y1) / e->a;
+		}
+		if (y2 > pymax) {
+			y2 = pymax;
+			x2 = (e->c - y2) / e->a;
+		}
+		if (y2 < pymin) {
+			y2 = pymin;
+			x2 = (e->c - y2) / e->a;
+		}
+	}
 
-    double a, b, c, s, area;
-    Site os1, os2;
+	double a, b, c, s, area;
+	Site os1, os2;
 
-    os1.coord.x = x1;
-    os1.coord.y = y1;
-    os2.coord.x = x2;
-    os2.coord.y = y2;
+	os1.coord.x = x1;
+	os1.coord.y = y1;
+	os2.coord.x = x2;
+	os2.coord.y = y2;
 
-    a = dist(e->reg[0], &os1);
-    b = dist(e->reg[0], &os2);
-    c = dist(&os1, &os2);
-    s = (a + b + c) / 2;
-    area = sqrt(s * (s - a) * (s - b) * (s - c));
-    //printf("%f %f %f -> ", a, b, c);
+	a = dist(e->reg[0], &os1);
+	b = dist(e->reg[0], &os2);
+	c = dist(&os1, &os2);
+	s = (a + b + c) / 2;
+	area = sqrt(s * (s - a) * (s - b) * (s - c));
+	//printf("%f %f %f -> ", a, b, c);
 
-    printf("%f,%f ", e->reg[0]->coord.x, e->reg[0]->coord.y);
-    printf("%f,%f %f,%f ", x1, y1, x2, y2);
-    printf("%f ", area);
-    printf("%s", e->reg[0]->text);
+	printf("%f,%f ", e->reg[0]->coord.x, e->reg[0]->coord.y);
+	printf("%f,%f %f,%f ", x1, y1, x2, y2);
+	printf("%f ", area);
+	printf("%s", e->reg[0]->text);
 
-    a = dist(e->reg[1], &os1);
-    b = dist(e->reg[1], &os2);
-    c = dist(&os1, &os2);
-    s = (a + b + c) / 2;
-    area = sqrt(s * (s - a) * (s - b) * (s - c));
-    //printf("%f %f %f -> ", a, b, c);
+	a = dist(e->reg[1], &os1);
+	b = dist(e->reg[1], &os2);
+	c = dist(&os1, &os2);
+	s = (a + b + c) / 2;
+	area = sqrt(s * (s - a) * (s - b) * (s - c));
+	//printf("%f %f %f -> ", a, b, c);
 
-    printf("%f,%f ", e->reg[1]->coord.x, e->reg[1]->coord.y);
-    printf("%f,%f %f,%f ", x1, y1, x2, y2);
-    printf("%f ", area);
-    printf("%s", e->reg[1]->text);
-    }
-
+	printf("%f,%f ", e->reg[1]->coord.x, e->reg[1]->coord.y);
+	printf("%f,%f %f,%f ", x1, y1, x2, y2);
+	printf("%f ", area);
+	printf("%s", e->reg[1]->text);
+}
